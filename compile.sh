dc079365c8045a811546d5d44097b28ec5ab5de8
장보우 Bowoo Jang <lisabowoo@gmail.com>
2018-12-16 19:50:37 +0900

    Update "compile.sh"
    Multi-compile parts only

--- compile.sh
+++ compile.sh
@@ -1,40 +1,21 @@
-for entry in ./p4
+#entry is folders in home directory
+for entry in ./*
 do
     if [ -d "$entry" ]; then
         cd $entry
+        #files is files in $entry directory
         for files in ./*
         do 
+            # reconstruct filename 
+            # check http://mindspill.net/computing/linux-notes/working-with-filenames-in-bash/
             NAME=$(echo "${files#*/}")
             newName=$(echo "${NAME%.*}")
             
             #IFS="_" read -ra NAMES <<< "$entry"
             #newName="${NAMES[1]}"
-
-            if [ -f "$newName.out" ]
-            then
-                for folder in ~/test/final/testcase/"${entry#*/}"
-                do
-                    var=1
-                    for tc in $folder/i*
-                    do
-                        outName=$(echo "${newName}_o$var")
-                        if [ -f "$outName" ]
-                        then
-                            echo "$outName already exits!"
-                            continue
-                        else
-                            echo "$outName compile"
-                            ./$newName.out < $tc > $outName 
-                            ((var++))
-                        fi
-                    done
-                done
-            else
-                echo "no compilation"
-                #gcc -o $newName.out $NAME
-            fi
+            
+            gcc -o $newName.out $NAME
         done
         cd ..
-        #sh ../testcase/a.sh
     fi
 done
